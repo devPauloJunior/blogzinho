fb62a36fe3e15dec5009a1af13596315101c3df7
Paulo Junior <npaulo.ce@gmail.com>
2023-03-21 18:26:45 -0300

    New hook Form configraded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+// MEUS CSS
 import './App.css';
+
+// MEUS IMPORTS
+import { BrowserRouter, Routes, Route, Navigate, Router } from 'react-router-dom';
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <div className='container'>
+          <Routes>
+            <Route path="/" element={ <Home /> } />
+            <Route path="/about" element={ <About /> } />
+            <Route path="/login" element={ <Login /> } />
+            <Route path="/register" element={ <Register /> } />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+// MEU CSS
+import styles from './NavBar.module.css';
+
+// MEUS IMPORTS
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav className={ styles.navbar }>
+        <NavLink to="/" className={ styles.brand }>
+            <span>Blogzinho</span>
+        </NavLink>
+
+        <ul className={ styles.link_list }>
+            <li> <NavLink to="/" className={ ({ isActive }) => ( isActive ? styles.active : "") }
+            > Home </NavLink> </li>
+            <li> <NavLink to="/login" className={ ({ isActive }) => ( isActive ? styles.active : "") }>Entrar</NavLink> </li>
+            <li> <NavLink to="/register" className={ ({ isActive }) => ( isActive ? styles.active : "") }>Cadastrar</NavLink> </li>
+            <li> <NavLink to="/about" className={ ({ isActive }) => ( isActive ? styles.active : "") }>Sobre</NavLink> </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
